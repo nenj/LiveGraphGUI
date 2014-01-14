cba0e4a33a34e207781d155fd5de9539721898ac
nenj <nenj.github@gmail.fake>
2014-01-14 15:59:19 +0100

    Source files created and README initialized

---
+++ LiveGraph.cpp
@@ -0,0 +1,8 @@
+//
+//  LiveGraph.cpp
+//
+//  Created by Nicolas Enjalbert on 14/01/2014.
+//
+//
+
+#include "LiveGraph.h"
---
+++ LiveGraph.h
@@ -0,0 +1,13 @@
+//
+//  LiveGraph.h
+//
+//  Created by Nicolas Enjalbert on 14/01/2014.
+//
+//
+
+#ifndef ____LiveGraph__
+#define ____LiveGraph__
+
+#include <iostream>
+
+#endif /* defined(____LiveGraph__) */
